5cbdeaa2338d373ed1174881414690c21a309918
Devan McGeer <devan@meshtrade.co>
2025-11-21 09:08:05 +0200

    feat(actions): My attempt at the epsilon greedy algorithm

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"math/rand/v2"
 	"net/http"
 	"os"
 )
@@ -19,6 +20,7 @@ const (
 	START_ENDPOINT  = "/api/mortys/start/"
 	PORTAL_ENDPOINT = "/api/mortys/portal/"
 	STATUS_ENDPOINT = "/api/mortys/status/"
+	EPSILON         = 0.4
 )
 
 var (
@@ -46,14 +48,6 @@ type MortySender interface {
 	Send(client *http.Client)
 }
 
-type Planet struct {
-	PlanetNumber       PlanetNumber
-	CurrentMortyAmount int
-	Survives           int
-	TotalSent          int
-	SurvivalRate       float32
-}
-
 type PlanetNumber int
 
 const (
@@ -62,9 +56,22 @@ const (
 	PurgePlanet
 )
 
+type Planet struct {
+	PlanetNumber       PlanetNumber
+	CurrentMortyAmount int
+	Survives           int
+	TotalSent          int
+	SurvivalRate       float32
+}
+
+type Action struct {
+	avgSurvivalRate     float32
+	survivalRateHistory []float32
+}
+
 func main() {
 	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelInfo,
+		Level: slog.LevelDebug,
 	})
 	logger := slog.New(handler)
 	slog.SetDefault(logger)
@@ -79,33 +86,51 @@ func main() {
 
 	mortiesCount := start.MortiesInCitadel
 
-	cobPlanet := Planet{
-		PlanetNumber:       0,
-		CurrentMortyAmount: 2,
-		Survives:           0,
-		SurvivalRate:       0,
-	}
-
-	cronenPlanet := Planet{
-		PlanetNumber:       1,
-		CurrentMortyAmount: 2,
-		Survives:           0,
-		SurvivalRate:       0,
-	}
-	purgePlanet := Planet{
-		PlanetNumber:       2,
-		CurrentMortyAmount: 2,
-		Survives:           0,
-		SurvivalRate:       0,
-	}
+	var actions = map[[3]int]*Action{{2, 2, 2}: {avgSurvivalRate: 0.1, survivalRateHistory: []float32{0.1}}}
 
 	for mortiesCount > 0 {
+		randomChance := rand.Float32()
+		slog.Debug("chance", "chance<epsilon", float32(randomChance) < EPSILON)
+		if float32(randomChance) < EPSILON {
+			slog.Debug("PERFORM RANDOM ACTION")
+			randCombo := RandomCombo()
 
-		cobPlanet.Send(client)
-		cronenPlanet.Send(client)
-		purgePlanet.Send(client)
+			if mortiesCount < 3 {
+				randCombo = [3]int{mortiesCount, 0, 0}
+			}
+			rate := Send(client, randCombo)
+			slog.Debug("best survival rate",
+				"randCombo", randCombo,
+				"rate with combo", rate,
+			)
+
+			if _, ok := actions[randCombo]; ok {
+				actions[randCombo].survivalRateHistory = append(actions[randCombo].survivalRateHistory, rate)
+				actions[randCombo].avgSurvivalRate = Average(actions[randCombo].survivalRateHistory)
+			} else {
+				actions[randCombo] = &Action{rate, []float32{rate}}
+			}
+		} else {
+			slog.Debug("PERFORM BEST PERFOMING ACTION")
+			bestCombo := FindBestSurvivalCombo(actions)
+			if mortiesCount < 3 {
+				bestCombo = [3]int{mortiesCount, 0, 0}
+			}
+			rate := Send(client, bestCombo)
+			slog.Debug("best survival rate",
+				"bestCombo", bestCombo,
+				"rate with combo", rate,
+			)
+			if _, ok := actions[bestCombo]; ok {
+				actions[bestCombo].survivalRateHistory = append(actions[bestCombo].survivalRateHistory, rate)
+				actions[bestCombo].avgSurvivalRate = Average(actions[bestCombo].survivalRateHistory)
+			} else {
+				actions[bestCombo] = &Action{rate, []float32{rate}}
+			}
+		}
 
 		status := GetEpisodeStatus(client)
+
 		rate := float32(status.MortiesOnPlanetJessica) / float32(1000)
 		slog.Info("Status",
 			"MortiesInCitadel",
@@ -126,86 +151,54 @@ type SendMorty struct {
 	MortyCount int `json:"morty_count"`
 }
 
-func (p *Planet) Send(client *http.Client) {
-	slog.Debug(
-		"send morties parameters",
-		"mortyCount", p.CurrentMortyAmount,
-		"planet", p.PlanetNumber,
-	)
-	p.TotalSent += p.CurrentMortyAmount
-	sm := &SendMorty{Planet: int(p.PlanetNumber), MortyCount: p.CurrentMortyAmount}
-	jsonBody, err := json.Marshal(*sm)
-	if err != nil {
-		slog.Error(err.Error())
+func Send(client *http.Client, combo [3]int) float32 {
+	var count int
+	var total int
+	for i, v := range combo {
+		sm := &SendMorty{Planet: i, MortyCount: v}
+		jsonBody, err := json.Marshal(*sm)
+		if err != nil {
+			slog.Error(err.Error())
+		}
+
+		bytesReader := bytes.NewReader(jsonBody)
+
+		req, err := http.NewRequest("POST", fmt.Sprintf("%s%s", BASE_URL, PORTAL_ENDPOINT), bytesReader)
+		if err != nil {
+			slog.Error(err.Error())
+		}
+
+		req.Header.Set("Authorization", AUTH_HEADER)
+		req.Header.Set("Content-Type", "application/json")
+
+		res, err := client.Do(req)
+		if err != nil {
+			slog.Error(err.Error())
+		}
+		defer res.Body.Close()
+
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			slog.Error("error reading response body",
+				"error", err.Error(),
+			)
+		}
+
+		portal := &Portal{}
+		err = json.Unmarshal(b, portal)
+		if err != nil {
+			slog.Error("error unmarshalling response body",
+				"error", err.Error(),
+				"response body", string(b),
+			)
+		}
+		total += v
+		if portal.Survived {
+			count += v
+		}
 	}
+	return float32(count) / float32(total)
 
-	bytesReader := bytes.NewReader(jsonBody)
-
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s%s", BASE_URL, PORTAL_ENDPOINT), bytesReader)
-	if err != nil {
-		slog.Error(err.Error())
-	}
-
-	req.Header.Set("Authorization", AUTH_HEADER)
-	req.Header.Set("Content-Type", "application/json")
-
-	res, err := client.Do(req)
-	if err != nil {
-		slog.Error(err.Error())
-	}
-	defer res.Body.Close()
-
-	b, err := io.ReadAll(res.Body)
-	if err != nil {
-		slog.Error("error reading response body",
-			"error", err.Error(),
-		)
-	}
-
-	portal := &Portal{}
-	err = json.Unmarshal(b, portal)
-	if err != nil {
-		slog.Error("error unmarshalling response body",
-			"error", err.Error(),
-			"response body", string(b),
-		)
-	}
-
-	slog.Debug("morties survived",
-		"planet", p.PlanetNumber,
-		"morties sent", p.CurrentMortyAmount,
-		"survived", portal.Survived,
-		"total survives", p.Survives,
-		"TotalPlanetJessicaCount", portal.MortiesOnPlanetJessica,
-	)
-	if portal.Survived {
-		p.Survives += p.CurrentMortyAmount
-	}
-	p.SurvivalRate = float32(p.Survives) / float32(p.TotalSent)
-
-	slog.Debug("morties survival rate",
-		"planet", p.PlanetNumber,
-		"total survives", p.Survives,
-		"total sent", p.TotalSent,
-		"SurvivalRate", p.SurvivalRate,
-	)
-	sr := p.SurvivalRate
-	if sr > 0.54 && sr <= 1.0 {
-		p.CurrentMortyAmount = 3
-	} else if sr > 0.46 && sr <= 0.54 {
-		p.CurrentMortyAmount = 2
-	} else {
-		p.CurrentMortyAmount = 1
-	}
-
-	if portal.MortiesInCitadel < 3 {
-		p.CurrentMortyAmount = portal.MortiesInCitadel
-	}
-	slog.Info("morties count",
-		"planet", p.PlanetNumber,
-		"current morty amount", p.CurrentMortyAmount,
-		"survival rate", p.SurvivalRate,
-	)
 }
 
 func StartEpisode(client *http.Client) Status {
@@ -271,4 +264,57 @@ func GetEpisodeStatus(client *http.Client) Status {
 	}
 
 	return *status
+}
+
+func RandomCombo() [3]int {
+	return [3]int{rand.IntN(3) + 1, rand.IntN(3) + 1, rand.IntN(3) + 1}
+}
+
+func FindMax(f []int) int {
+	slog.Debug("FIND MAX FLOAT", "length of slice", len(f))
+	if len(f) == 0 {
+		return 0
+	}
+
+	highest := f[0]
+	for _, v := range f {
+		if v > highest {
+			highest = v
+		}
+	}
+	slog.Debug("FindMaxFloat", "values", f, "highest", highest)
+	return highest
+}
+func FindBestSurvivalCombo(actions map[[3]int]*Action) [3]int {
+	slog.Debug("FindBestSurvivalCombo")
+	var highest float32
+	var bestCombo [3]int
+	if len(actions) == 0 {
+		return [3]int{rand.IntN(3) + 1, rand.IntN(3) + 1, rand.IntN(3) + 1}
+	}
+	for i, v := range actions {
+		if v.avgSurvivalRate > highest {
+			highest = v.avgSurvivalRate
+			bestCombo = i
+		}
+
+	}
+	slog.Debug("returned combo", "bestCombo", bestCombo)
+	return bestCombo
+}
+
+func Average(f []float32) float32 {
+	if len(f) == 0 {
+		return 0
+	}
+
+	var total float32
+	var count int
+	for _, v := range f {
+		count++
+		total += v
+	}
+
+	return total / float32(count)
+
 }
